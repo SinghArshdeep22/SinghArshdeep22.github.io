30e26ac299cc22f69e97b0f83e0e7bfd9f0ae98d
singh <arshsngh05@gmail.com>
2024-07-08 14:05:09 +0200

    Adding contact page

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 "use strict"
 
+// Hamburger Menu
 const hamburger = document.getElementById('hamburger');
 const navContainer = document.getElementById('nav-container');
 
 hamburger.addEventListener('click', () => {
     navContainer.classList.toggle('show');
+});
+
+// Contact - Send
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.contact-form');
+    const confirmationMessage = document.getElementById('confirmation-message');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        confirmationMessage.style.display = 'block';
+        form.reset();
+    });
 });
